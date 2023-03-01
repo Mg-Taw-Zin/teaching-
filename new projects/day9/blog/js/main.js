f2efb2afc0c31ef2ff2cba0b47abe810810a9fb8
Mg-Taw-Zin <kyawkyaw91a@gmail.com>
2023-03-01 19:41:32 +0630

    3/1/2023

--- new projects/day9/blog/js/main.js
+++
@@ -1,6 +0,0 @@
-let bars = document.querySelector("#bars");
-let nav_links = document.querySelector(".nav-links");
-bars.addEventListener("click", function (){
-    nav_links.classList.toggle("show");
-    bars.classList.toggle("fa-times");
-})
